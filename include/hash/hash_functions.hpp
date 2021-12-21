96528254f45f41c28e1d26e49e5ffbd507681c7e
Andrey <andrey_nuzhnov.01@mail.ru>
2021-12-21 19:30:03 +0700

    Updated hash functions

--- include/hash/hash_functions.hpp
+++ include/hash/hash_functions.hpp
@@ -10,28 +10,28 @@
 // Functions which translate hash_val to index in hash table
 // This functions has two parameters:
 // 		1) hash_val - value after hashing key
-//		2) table_size - size of hash table
+//		2) buckets_count - count of buckets in hash table
 
 // Multiplicative hash function
-size_t mul_hash(size_t hash_val, size_t table_size);
+size_t mul_hash(size_t hash_val, size_t buckets_count);
 // Modular hash function
-size_t mod_hash(size_t hash_val, size_t table_size);
+size_t mod_hash(size_t hash_val, size_t buckets_count);
 
 
 // Probing functions for open addressing hash tables.
 // This functions has following parameters:
 //		1) hash_val - result of functions "mul_hash" or "mod_hash"
 //		2) index - number of probe
-//		3) table_size - size of hash table
+//		3) buckets_count - count of buckets in hash table
 
 // Linear probing function
-size_t linear_probing(size_t hash_val, size_t index, size_t table_size);
+size_t linear_probing(size_t hash_val, size_t index, size_t buckets_count);
 // Quadratic probing function
-size_t quadratic_probing(size_t hash_val, size_t index, size_t table_size);
+size_t quadratic_probing(size_t hash_val, size_t index, size_t buckets_count);
 // Double probing
 // other_hash_value - is a hash value obtained by another hashing function
 size_t double_probing(size_t hash_val, size_t other_hash_val,
-						size_t index, size_t table_size);
+						size_t index, size_t buckets_count);
 
 
 #endif  // _HASH_FUNCTIONS_
--- src/hash/hash_functions.cpp
+++ src/hash/hash_functions.cpp
@@ -1,33 +1,33 @@
 #include <hash/hash_functions.hpp>
 
 
-size_t mul_hash(size_t hash_val, size_t table_size)
+size_t mul_hash(size_t hash_val, size_t buckets_count)
 {
 	// The golden ratio
 	constexpr auto PHI = 0.61803398874989484820;
 	double frac = hash_val * PHI - static_cast<size_t>(hash_val * PHI);
 
-	return static_cast<size_t>(table_size * frac);
+	return static_cast<size_t>(buckets_count * frac);
 }
 
-size_t mod_hash(size_t hash_val, size_t table_size)
+size_t mod_hash(size_t hash_val, size_t buckets_count)
 {
-	return static_cast<size_t>(hash_val % table_size);
+	return static_cast<size_t>(hash_val % buckets_count);
 }
 
 
-size_t linear_probing(size_t hash_val, size_t index, size_t table_size)
+size_t linear_probing(size_t hash_val, size_t index, size_t buckets_count)
 {
-	return (hash_val + index) % table_size;
+	return (hash_val + index) % buckets_count;
 }
 
-size_t quadratic_probing(size_t hash_val, size_t index, size_t table_size)
+size_t quadratic_probing(size_t hash_val, size_t index, size_t buckets_count)
 {
-	return (hash_val + index * index) % table_size;
+	return (hash_val + index * index) % buckets_count;
 }
 
 size_t double_probing(size_t hash_val, size_t other_hash_val,
-						size_t index, size_t table_size)
+						size_t index, size_t buckets_count)
 {
-	return (hash_val + other_hash_val * index) % table_size;
+	return (hash_val + other_hash_val * index) % buckets_count;
 }
